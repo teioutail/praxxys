e220481f34e4b1e7053682863a7929d805a83732
teioutail <teioutail@gmail.com>
2021-08-08 22:18:05 +0800

    update ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,14 +1,45 @@
 <?php
 
+// namespace Database\Factories;
+
+// use Faker\Generator as Faker;
+
+// $factory->define(\App\Product::class, function (Faker $faker){
+//     return [
+//         'name' => $faker->name(),
+//         'category' => 'Manga',
+//         'description' => $faker->paragraph(),
+//         'date_time' => null
+//     ];
+// });
+
 namespace Database\Factories;
 
-use Faker\Generator as Faker;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-$factory->define(\App\Product::class, function (Faker $faker){
-    return [
-        'name' => $faker->name(),
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+        'name' => $this->faker->name(),
         'category' => 'Manga',
-        'description' => $faker->paragraph(),
+        'description' => $this->faker->paragraph(),
         'date_time' => null
-    ];
-});
+        ];
+    }
+}
